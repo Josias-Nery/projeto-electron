373de20d03803271eb2c9c372dfaec6bfbbac1de
Lil Nery <josias.n@hotmail.com>
2022-08-09 19:46:49 -0300

    projeto-electron-vue

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-toolbar-title>Most Used Words</v-toolbar-title>
+    </v-app-bar>
+    <v-content>
+        <TodoHome />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import TodoHome from './components/TodoHome.vue';
+export default {
+  name: 'App',
+
+  components: {
+    TodoHome
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
